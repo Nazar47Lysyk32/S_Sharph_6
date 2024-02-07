487a99b1bf11de96dd3ad99fc109b0e94e66b6c6
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-02-07 19:20:47 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Використання лямбда-виразу для виведення лише парних чисел
+        var evenNumbers = Array.FindAll(numbers, num => num % 2 == 0);
+
+        // Виведення результатів
+        Console.WriteLine("Парні числа:");
+        foreach (var number in evenNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
