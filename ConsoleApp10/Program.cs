470439ee5343e8624b5e4822d2071329e4ee12d8
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-02-07 19:22:21 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numbers = { 1.5, 2.7, 3.9, 4.2, 5.1 };
+
+        // Використання лямбда-виразу для обчислення середнього значення
+        double average = Array.Exists(numbers, num => true) ? numbers.Average() : 0;
+
+        // Виведення результату
+        Console.WriteLine("Середнє значення: " + average);
+    }
+}
